08fc5b860907418951de155bdf9ef025a51d0dfb
Kim.WonKyu <develobe@gmail.com>
2017-08-04 17:45:57 +0900

    merge updated

--- DevelobeGithub/ViewController.swift
+++ DevelobeGithub/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("test for first commit")
+        print("test for hotfix branch!!!")
         
         // Do any additional setup after loading the view, typically from a nib.
     }
